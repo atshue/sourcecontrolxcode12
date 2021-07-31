d5ef172ec69492585e8ea36453063350497f5f1f
Alexis Tie-Shue <atieshue@gmail.com>
2021-07-31 14:58:44 -0400

    Initial Commit

---
+++ SourceControlXcode12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControlXcode12
+//
+//  Created by Alexis Tie-Shue on 7/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Alexis Tie-Shue on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
